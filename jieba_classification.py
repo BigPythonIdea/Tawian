d238ccb6b014c6599679318e8517143b8994f902
Mooncat <48311280+klas9802@users.noreply.github.com>
2021-04-18 13:48:21 +0800

    Update jieba_classification.py

--- jieba_classification.py
+++ jieba_classification.py
@@ -1,15 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 10:28:05 2021
+
+@author: Solana
+"""
+
 import pandas as pd
 import jieba
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 
+name = "其他"
+year = "2011"
 
-dt = pd.read_csv("光電.csv", index_col=0)
+dt = pd.read_csv(name+".csv", index_col=0)
 seqs = [[],[]]
 for t in dt["代碼"]:
     try:
-        df = pd.read_csv("zip/2011/"+str(t)+".csv").to_string()
+        df = pd.read_csv("zip/"+year+"/"+str(t)+".csv").to_string()
     except:
         pass
 
@@ -27,3 +36,7 @@ for t in dt["代碼"]:
     seq = tok.texts_to_sequences(st)
     seqs[0].append(t)
     seqs[1].append(seq)
+
+dd = pd.DataFrame(seqs).T
+dd.to_csv(name+year+".csv")
+
