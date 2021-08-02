fdce1f73686bae12ab5ea55d4f49919df8070b29
Mooncat <48311280+klas9802@users.noreply.github.com>
2021-08-02 13:07:59 +0800

    Update doc4.py

--- doc4.py
+++ doc4.py
@@ -3,38 +3,85 @@
 Created on Wed Jul 14 13:44:06 2021
 
 @author: Mooncat
-ind = []
-for d in dirs:
-    ind.append(PATH+"\\"+d)
 
-row = []
-for i in ind:
-    ind_path = os.listdir(i)
-    for r in ind_path:
-        dfx = pd.read_csv(i+"\\"+r)
-        r = int(str(r).replace(".csv", ""))
-        content = str(dfx["Year"])
-        print(content)
-        break
+1. a b = 1
+2. not b
 """
 
 import pandas as pd
 import os
 
-PATH = 'C:\\Users\Mooncat\py\TawianLab\Industry'
+
+INDUSTRY = '化學'
+PATH = 'C:\\Users\Mooncat\py\TawianLab\Industry'+'\\'+INDUSTRY
 dirs = os.listdir(PATH)
 
-df = pd.read_csv("Taiwans.csv")
+key = []
+for dirs_ in dirs:
+    key.append(int(dirs_[0:4]))
+
+df = pd.read_csv("T16.csv")
 a = df["Content"][108]
 b = df["Content"][129]
 
-Y_16 = []
-Y_17 = []
-Y_18 = []
-Y_19 = []
-Y_20 = []
 
-dfx = pd.read_csv(PATH+"\\化學\\1708.csv")
+point = []
+other = []
+
+
+for i in key:
+    ga = df[(df.Symbol == i) & (df.Content == a)].index.tolist()
+    point.append(ga)
+    gb = df[(df.Symbol == i) & (df.Content == b)].index.tolist()
+    if not gb:
+        print(i)
+    other.append(gb)
+
+
+def article(star,end,k):
+    lst = []
+    for i in range(star,end):
+        lst.append(df["Content"][i])
+    dt = pd.DataFrame(lst)
+    # dt.to_csv(str(k)+".csv", encoding="utf_8_sig")
+        
+
+
+
+p = []
+for s in point:
+    for r in s:
+        p.append(r)
+    
+o = []
+for s in other:
+    for r in s:
+        o.append(r)
+
+lst = []
+length = len(p)+1
+
+try:
+    for i in range(length-1):
+        lst.append(p.pop(0))
+        lst.append(o.pop(0))
+    print(lst)
+except IndexError:
+    print("IndexError")
+
+
+for k in key:
+    for i in range(len(lst)):
+        try:
+            article(lst[i], lst[i+1],k)
+        except IndexError:
+            break
+    
+
+
+        
+        
+
 
 
 
