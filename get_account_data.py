cae575489aed87471d81e17d4c202aca540a05b8
klas9802 <klas9806@gmail.com>
2023-05-01 10:59:49 +0800

    Complete the database task.

--- get_account_data.py
+++ get_account_data.py
@@ -1,34 +1,46 @@
-import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import pymysql
 import time
 
-# 資料庫連線設定
-host = "localhost"
-user = "root"
-password = "123456"
-database = "stockdb"
 
-data = []
-names = []
-# 資料庫連線建立
-conn = pymysql.connect(host=host, user=user, password=password, database=database)
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM stock')
-result = cursor.fetchall()
-# 將每一行資料轉換成字典型別，並顯示出來
-for row in result:
-    data.append(row[0])
-    names.append(row[1])
-# 關閉資料庫連接
-conn.close()
-data = [int(x) for x in data]
+def connect_database():
+    host = "localhost"
+    user = "root"
+    password = "123456"
+    database = "stockdb"
 
-for symbol in data:
-    driver = webdriver.Chrome()
-    driver.get("https://mops.twse.com.tw/mops/web/t163sb03")
+    conn = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM stock')
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def insert_report_to_database(company_code, quarter_date, content, year):
+    host = "localhost"
+    user = "root"
+    password = "123456"
+    database = "stockdb"
+
+    conn = pymysql.connect(host=host, user=user, password=password, database=database)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO quarterly_reports (company_code, quarter_date, content, year) VALUES (%s, %s, %s, %s)', (company_code, quarter_date, content, year))
+    conn.commit()
+    conn.close()
+
+
+def get_report_content(symbol, year, season):
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(executable_path = 'Path to webdriver', options=options)
+        driver.get("https://mops.twse.com.tw/mops/web/t163sb03")
+    except Exception as e:
+        print(e)
+    
     try:
         select = Select(driver.find_element(By.XPATH, '//*[@id="isnew"]'))
         select.select_by_value("false")
@@ -36,22 +48,58 @@ for symbol in data:
         element = driver.find_element(By.XPATH, '//*[@id="co_id"]')
         element.send_keys(str(symbol))
 
-        year = driver.find_element(By.XPATH, '//*[@id="year"]')
-        year.send_keys("109")
+        year_field = driver.find_element(By.XPATH, '//*[@id="year"]')
+        year_field.clear() # 先清空欄位內容
+        year_field.send_keys(str(year))
 
-        season = Select(driver.find_element(By.XPATH, '//*[@id="season"]'))
-        season.select_by_value("01")
+        season_select = Select(driver.find_element(By.XPATH, '//*[@id="season"]'))
+        season_select.select_by_value(str(season))
 
-        button = driver.find_element(By.XPATH, '/html/body/center/table/tbody/tr/td/div[4]/table/tbody/tr/td/div/table/tbody/tr/td[3]/div/div[3]/form/table/tbody/tr/td[4]/table/tbody/tr/td[2]/div/div/input').click()
+        search_button = driver.find_element(By.XPATH, '/html/body/center/table/tbody/tr/td/div[4]/table/tbody/tr/td/div/table/tbody/tr/td[3]/div/div[3]/form/table/tbody/tr/td[4]/table/tbody/tr/td[2]/div/div/input').click()
     except Exception as e:
         print(str(symbol))
         print(e)
+        driver.quit()
+        return ""
+
     time.sleep(3)
-    content = driver.find_element(By.XPATH, '//*[@id="table01"]/table[4]/tbody/tr[4]/td/pre').text
+    try:
+        content = driver.find_element(By.XPATH, '//*[@id="table01"]/table[4]/tbody/tr[4]/td/pre').text
+    except Exception as e:
+        content = ""
+        print(str(symbol) + " - No Data")
 
-    print(content)
+    driver.quit()
+    return content
 
+
+def main():
+    result = connect_database()
+    symbols = [row[0] for row in result]
+    symbols = list(map(int, symbols))
+
+    years = ['110','111']
+    seasons = ['01','02','03','04']
     
-    #driver.close()
-    break
+    for year in years:
+        for season in seasons:
+            for symbol in symbols:
+                try:
+                    content = get_report_content(symbol, year, season)
+                    insert_report_to_database(symbol, season, content, year)
+                except Exception as e:
+                    with open('example.txt', 'w') as file:
+                        file.write(symbol)
+                        file.write(e)
+                    print('文字已寫入至 error.txt 檔案')
 
+
+    from datetime import datetime
+    currentDateAndTime = datetime.now()
+    currentTime = currentDateAndTime.strftime("%H:%M:%S")
+    print("The current time is", currentTime)
+
+
+
+if __name__ == '__main__':
+    main()
